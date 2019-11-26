67437c3820d5ee87d90db65721a1f2391edb67d1
unwarysage <shawx538@morris.umn.edu>
2019-11-26 16:23:30 -0600

    Fix routing table, add route for hero-list
    add standard-edn-response wrapper
    make routing table hot reloadable
    fix misalligned brackets in routing table
    add hero-list api route

--- src/clj/dbnd/data.clj
+++ src/clj/dbnd/data.clj
@@ -1,25 +1,24 @@
 (ns dbnd.data
   (:require [next.jdbc :as jdbc]
-            [clojure.edn :as edn]
-            ))
+            [clojure.edn :as edn]))
 (def credentials (edn/read-string (slurp "credentials.edn")))
 
 (def db
   (merge
-    {:dbtype "mysql" 
-     :dbname "DBnD"
-     :user "example"
-     :password "abc123notapassword"     
-     :host "ids"
-     :serverTimezone "America/Chicago"}
-    credentials))
+   {:dbtype "mysql"
+    :dbname "DBnD"
+    :user "example"
+    :password "abc123notapassword"
+    :host "ids"
+    :serverTimezone "America/Chicago"}
+   credentials))
 
 (def ds (jdbc/get-datasource db))
 
 (defn get-hero-names []
   (into #{}
-        (map 
-          #(hash-map 
-             :hero-name (:Heroes/Hero_Name %) 
-             :hero-id (:Heroes/Hero_ID %))) 
+        (map
+         #(hash-map
+           :hero-name (:Heroes/Hero_Name %)
+           :hero-id (:Heroes/Hero_ID %)))
         (jdbc/plan ds ["select * from Heroes"])))
