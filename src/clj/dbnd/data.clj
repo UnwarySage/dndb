18ba8ee0d7cbad433a6ba21038866ac59312f502
unwarysage <shawx538@morris.umn.edu>
2019-12-02 09:55:37 -0600

    Add selecting specific hero

---
+++ src/clj/dbnd/data.clj
@@ -0,0 +1,32 @@
+(ns dbnd.data
+  (:require [next.jdbc :as jdbc]
+            [clojure.edn :as edn]))
+(def credentials (edn/read-string (slurp "credentials.edn")))
+
+(def db
+  (merge
+   {:dbtype "mysql"
+    :dbname "DBnD"
+    :user "example"
+    :password "abc123notapassword"
+    :host "ids"
+    :serverTimezone "America/Chicago"}
+   credentials))
+
+(def ds (jdbc/get-datasource db))
+
+
+
+(defn get-hero-names []
+  (into #{}
+        (map
+         #(hash-map
+           :hero-name (:Heroes/Hero_Name %)
+           :hero-id (:Heroes/Hero_ID %)))
+        (jdbc/plan ds ["select * from Heroes"])))
+
+(defn get-hero-data 
+  [hero-id]
+  (let [command (str "select * from Heroes where Hero_ID = " hero-id ";")
+        _ (println command)]
+    (jdbc/execute-one! ds [command])))
